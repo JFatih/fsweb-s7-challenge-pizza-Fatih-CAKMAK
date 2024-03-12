17f8a5017950c873332acd72bca739b8b7fdc8b1
fatih-cakmak <cakmak.35f@gmail.com>
2024-03-12 16:36:00 +0300

    footer menu done

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,49 @@ function App() {
           <SuccessPage orderForm={orderForm} />
         </Route>
       </Switch>
+      <footer className="footer-bottom">
+        <section className="footer-data">
+          <div className="footer-info">
+            <h3>Teknolojik Yemekler</h3>
+            <div>
+              <img src="Assets/mile2-aseets/footer/icons/icon-1.png" />
+              <p>341 Londonderry Road, Istanbul Türkiye</p>
+            </div>
+            <div>
+              <img src="Assets/mile2-aseets/footer/icons/icon-2.png" />
+              <p>aciktim@teknolojikyemekler.com</p>
+            </div>
+            <div>
+              <img src="Assets/mile2-aseets/footer/icons/icon-3.png" />
+              <p>+902161234567</p>
+            </div>
+          </div>
+          <div className="footer-menu">
+            <h4>Sıccacık Menuler</h4>
+            <p>Terminal Pizza</p>
+            <p>5 Kişilik Hacketlon Pizza</p>
+            <p>useEffect Tavuklu Pizza</p>
+            <p>Beyaz Console Frosty</p>
+            <p>Testler Geçti Mutlu Burger</p>
+            <p>Position Absolute Acı Burger</p>
+          </div>
+        </section>
+        <div className="footer-social">
+          <p>Instagram</p>
+          <div>
+            <img src="Assets/mile2-aseets/footer/insta/li-0.png" />
+            <img src="Assets/mile2-aseets/footer/insta/li-1.png" />
+            <img src="Assets/mile2-aseets/footer/insta/li-2.png" />
+            <img src="Assets/mile2-aseets/footer/insta/li-3.png" />
+            <img src="Assets/mile2-aseets/footer/insta/li-4.png" />
+            <img src="Assets/mile2-aseets/footer/insta/li-5.png" />
+          </div>
+        </div>
+      </footer>
+      <div className="grey-line"></div>
+      <div className="footer-copyright">
+        <p>© 2023 Teknolojik Yemekler</p>
+      </div>
     </div>
   );
 }
